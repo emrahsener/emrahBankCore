7bad43f61802dfc497cef21be1e522cbfa879f44
Emrah Sener <emrsener@gmail.com>
2018-09-10 00:16:03 +0100

    Initial Commit

---
+++ src/main/java/com/emrahbank/core/emrahBankCore/EmrahBankCoreApplication.java
@@ -0,0 +1,12 @@
+package com.emrahbank.core.emrahBankCore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmrahBankCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmrahBankCoreApplication.class, args);
+	}
+}
